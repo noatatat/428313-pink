0844d73f59e92d63266132633dece0a61eb53a61
Vladimir Vorobjev <C:\Users\vorobjev\AppData\Roaming\The Bat!>
2017-10-19 12:48:25 +0300

    исправление ошибок

--- js/yandex-map.js
+++ js/yandex-map.js
@@ -6,16 +6,16 @@ function init(){
     center: [59.938631, 30.323055],
     zoom: 17
   });
-myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {hintContent: "ул. Большая Конюшенная, 19/8"}, {
-  preset: "islands#redCircleDotIcon"
-});
-myMap.geoObjects.add(myPlacemark);
-myMap.controls.remove("geolocationControl");
-myMap.controls.remove("searchControl");
-myMap.controls.remove("trafficControl");
-myMap.controls.remove("typeSelector");
-myMap.controls.remove("fullscreenControl");
-myMap.controls.remove("rulerControl");
-myMap.controls.remove("zoomControl");
-myMap.controls.remove("RouteButton");
+  myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {hintContent: "ул. Большая Конюшенная, 19/8"}, {
+    preset: "islands#redCircleDotIcon"
+  });
+  myMap.geoObjects.add(myPlacemark);
+  myMap.controls.remove("geolocationControl");
+  myMap.controls.remove("searchControl");
+  myMap.controls.remove("trafficControl");
+  myMap.controls.remove("typeSelector");
+  myMap.controls.remove("fullscreenControl");
+  myMap.controls.remove("rulerControl");
+  myMap.controls.remove("zoomControl");
+  myMap.controls.remove("RouteButton");
 }
